625fbbb479e0ffd3c91b76b1fbcad542185a00c0
nmikstas <nmikstas@alumni.scu.edu>
2020-04-21 11:43:26 -0600

    Worked on the documentation format

--- assets/js/documentation.js
+++ assets/js/documentation.js
@@ -3,5 +3,18 @@
 
 $(document).ready(() =>
 {
+    //Scroll the navbar with the document.
+    $(window).scroll(() =>
+    {
+        let headerHeight = $(".header-class").outerHeight();
+        let scrollLocation = $(this).scrollTop();
+        let containerPosition = $(".doc-container").offset().top;
+        let navPadding = (scrollLocation > containerPosition) ? scrollLocation : containerPosition;
+        $(".sidenav").css({paddingTop: navPadding - headerHeight + 16, left: 200});        
+    });
+    
+
+
+    
    
-});
+})

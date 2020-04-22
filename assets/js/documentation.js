829c718d04986f6bb31b384cb8064d02893d83eb
nmikstas <nmikstas@alumni.scu.edu>
2020-04-21 19:54:25 -0600

    Continued tp work on the documentation styling

--- assets/js/documentation.js
+++ assets/js/documentation.js
@@ -1,20 +1,80 @@
+let scrollLink, scrollArray, subsections;
 
+let doScroll = () =>
+{
+    //Scroll the navbar with the document.
+    let headerHeight = $(".header-class").outerHeight();
+    let scrollLocation = $(this).scrollTop();
+    let containerPosition = $(".doc-container").offset().top;
+    let navPadding = (scrollLocation > containerPosition) ? scrollLocation : containerPosition;
+    $(".sidenav").css({marginTop: navPadding - headerHeight, left: 200}); 
+    
+    let index = 0;
+    let latched = false;
 
+    for(let i = 0; i < scrollLink.length; i++)
+    {
+        let sectionOffset = $(scrollLink[i].hash).offset().top - 20;
+
+        if (sectionOffset >= scrollLocation)
+        {
+            if(!latched)
+            {
+                latched = true;
+                index = i - 1;
+            }
+        }
+    }
+
+    let parentDiv;
+
+    for(let i = 0; i < scrollLink.length; i++)
+    {
+        if(index === i)
+        {
+            $(scrollLink[i]).addClass("active");
+            if($(scrollLink[i]).hasClass("subsection-link"))
+            {
+                parentDiv = $(scrollLink[i]).parent().attr("id");
+            }
+            else
+            {
+                parentDiv = $(scrollLink[i]).attr("subclass");
+            }
+        }
+        else
+        {
+            $(scrollLink[i]).removeClass("active");
+        }
+    }
+
+    for(let i = 0; i < subsections.length; i++)
+    {
+        thisDiv = $(subsections[i]).attr("id");
+        if(parentDiv === thisDiv)
+        {
+            $(subsections[i]).addClass("show-div");
+            $(subsections[i]).removeClass("hide-div");
+        }
+        else
+        {
+            $(subsections[i]).addClass("hide-div");
+            $(subsections[i]).removeClass("show-div");
+        }
+    }
+    
+    
+}
 
 $(document).ready(() =>
 {
-    //Scroll the navbar with the document.
-    $(window).scroll(() =>
-    {
-        let headerHeight = $(".header-class").outerHeight();
-        let scrollLocation = $(this).scrollTop();
-        let containerPosition = $(".doc-container").offset().top;
-        let navPadding = (scrollLocation > containerPosition) ? scrollLocation : containerPosition;
-        $(".sidenav").css({paddingTop: navPadding - headerHeight + 16, left: 200});        
-    });
-    
+    scrollLink = $(".scroll-link");
+    scrollArray = new Array(scrollLink.length);
+    subsections = $(".subsection");
+
+    $(window).scroll(() => doScroll());
+    doScroll();
+});
 
 
-    
-   
-})
+

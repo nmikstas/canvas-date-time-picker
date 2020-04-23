2277af510c9c9b324572e004791e3724a6ec81dd
nmikstas <nmikstas@alumni.scu.edu>
2020-04-23 00:00:30 -0600

    Added the documentation for all the utility functions

--- assets/js/documentation.js
+++ assets/js/documentation.js
@@ -62,8 +62,6 @@ let doScroll = () =>
             $(subsections[i]).removeClass("show-div");
         }
     }
-    
-    
 }
 
 $(document).ready(() =>
@@ -76,5 +74,89 @@ $(document).ready(() =>
     doScroll();
 });
 
+let calcEaster = () =>
+{
+    let year = parseInt($("#easter-text").val());
+    if(!year) return;
+    let date = CanvDTP.getEaster(year);
+    if(date.month === -1)
+    {
+        $("#easter-month").text(-1);
+        $("#easter-day").text(-1);
+        return;
+    }
+    $("#easter-month").text(date.month === 3 ? "March" : "April");
+    $("#easter-day").text(date.day);
+}
 
+let calcThanksgiving = () =>
+{
+    let year = parseInt($("#thanksgiving-text").val());
+    if(!year) return;
+    let date = CanvDTP.getThanksgiving(year);
+    $("#thanksgiving-month").text("November");
+    $("#thanksgiving-day").text(date.day);
+}
 
+let calcMLK = () =>
+{
+    let year = parseInt($("#mlk-text").val());
+    if(!year) return;
+    let date = CanvDTP.getMLK(year);
+    $("#mlk-month").text("January");
+    $("#mlk-day").text(date.day);
+}
+
+let calcMothers = () =>
+{
+    let year = parseInt($("#mothers-text").val());
+    if(!year) return;
+    let date = CanvDTP.getMothers(year);
+    $("#mothers-month").text("May");
+    $("#mothers-day").text(date.day);
+}
+
+let calcFathers = () =>
+{
+    let year = parseInt($("#fathers-text").val());
+    if(!year) return;
+    let date = CanvDTP.getFathers(year);
+    $("#fathers-month").text("June");
+    $("#fathers-day").text(date.day);
+}
+
+let calcWashington = () =>
+{
+    let year = parseInt($("#washington-text").val());
+    if(!year) return;
+    let date = CanvDTP.getWashington(year);
+    $("#washington-month").text("January");
+    $("#washington-day").text(date.day);
+}
+
+let calcMemorial = () =>
+{
+    let year = parseInt($("#memorial-text").val());
+    if(!year) return;
+    let date = CanvDTP.getMemorial(year);
+    $("#memorial-month").text("May");
+    $("#memorial-day").text(date.day);
+}
+
+let calcLabor = () =>
+{
+    let year = parseInt($("#labor-text").val());
+    if(!year) return;
+    let date = CanvDTP.getLabor(year);
+    $("#labor-month").text("September");
+    $("#labor-day").text(date.day);
+}
+
+let calcColumbus = () =>
+{
+    let year = parseInt($("#columbus-text").val());
+    if(!year) return;
+    let date = CanvDTP.getColumbus(year);
+    $("#columbus-month").text("October");
+    $("#columbus-day").text(date.day);
+}
